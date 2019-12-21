cb99c74605cd956d0fedb100a8fb26a921dfdc65
abygcreation <alembic-lets-cocksure6>
2019-12-21 18:22:02 +0100

    Review des premieres pages

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,14 +2,14 @@
   <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
     <div class="w-100">
       <h1 class="mb-0">
-        Loke
-        <span class="text-primary">Carlsson</span>
+        David
+        <span class="text-primary">RAMBEAU</span>
       </h1>
       <div class="subheading mb-5">
-        9695 Foo street · Bar town, CO 93440 · (123) 456-7890 ·
+        19 rue de SAUX, 54113 Gye, FRANCE
         <a
-          href="mailto:name@email.com"
-        >name@email.com</a>
+          href="mailto:david.rambeau@gmail.com"
+        >david.rambeau@gmail.com</a>
       </div>
       <p
         class="lead mb-5"
@@ -23,19 +23,19 @@
         >Github repo</a>.
       </p>
       <div class="social-icons">
-        <a href="https://linkedin.com/in/lokecarlsson" target="_blank" rel="noopener noreferrer">
+        <a href="https://linkedin.com/in/davidrambeau" target="_blank" rel="noopener noreferrer">
           <font-awesome :icon="['fab', 'linkedin']"/>
         </a>
-        <a href="https://github.com/LokeCarlsson" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/abygcreation" target="_blank" rel="noopener noreferrer">
           <font-awesome :icon="['fab', 'github']"/>
         </a>
-        <a href="https://twitter.com/carlssonloke" target="_blank" rel="noopener noreferrer">
+        <a href="https://twitter.com/s" target="_blank" rel="noopener noreferrer">
           <font-awesome :icon="['fab', 'twitter']"/>
         </a>
-        <a href="https://www.facebook.com/carlssonloke" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.facebook.com/s" target="_blank" rel="noopener noreferrer">
           <font-awesome :icon="['fab', 'facebook']"/>
         </a>
-        <a href="https://loke.dev" target="_blank" rel="noopener noreferrer">
+        <a href="https://david.rambeau.dev" target="_blank" rel="noopener noreferrer">
           <font-awesome :icon="['fas', 'rss']"/>
         </a>
       </div>
